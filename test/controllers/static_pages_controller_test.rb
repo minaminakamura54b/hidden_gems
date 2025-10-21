bc4ad23d4ee184b5463034f384be0682bc1af51e
Minami Nakamura <nakamura0373@gmail.com>
2025-10-20 21:08:17 -0700

    orgamize layoiut using boostrap and paeshal. finish chapter 5.1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Hidden gems app"
+    assert_select "title", "Hidden gems app"
   end
 
   test "should get help" do
